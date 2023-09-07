c78ff2cba93668e33285007225bd5e4b3461c40e
Mohamed Alsayed <93587781+AlsayedMo@users.noreply.github.com>
2023-09-07 12:06:35 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,27 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <!-- Stijlregel om het 'strong'-element op te maken -->
     <style>
         strong {
-            font-weight: 800;
-            font-size: 1.4rem;
+            font-weight: 800;    /* Vetgedrukte tekst */
+            font-size: 1.4rem;   /* Lettergrootte */
         }
     </style>
 </head>
 <body>
 <?php
 for ($i = 33; $i <= 543; $i++) {
-    echo '<p>';
+    echo '<p>'; // Start een nieuw alinea-element
     if ($i % 5 == 0 && $i % 9 == 0) {
-        echo ' <strong> Web </strong>';
+        echo ' <strong> Web </strong>'; // Als deelbaar door 5 EN 9, toon 'Web' vetgedrukt
     } elseif ($i % 5 == 0) {
-        echo '<strong> Software </strong>';
+        echo '<strong> Software </strong>'; // Als deelbaar door 5, toon 'Software' vetgedrukt
     } elseif ($i % 9 == 0) {
-        echo '<strong> Developer </strong> ' ;
+        echo '<strong> Developer </strong> '; // Als deelbaar door 9, toon 'Developer' vetgedrukt
     } else {
-        echo $i;
+        echo $i; // Anders toon het getal zelf
     }
-    echo '</p>';
+    echo '</p>'; // Sluit het alinea-element
 }
 ?>
 </body>
